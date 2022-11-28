2d8a8d9fd90f8e4605b37eac3d06c2f9575ba88c
Matthew Berlinic <mjb855@msn.com>
2022-11-28 12:40:50 -0600

    Added matplotlib back in and successfully ran

--- MattMain.py
+++ MattMain.py
@@ -1,9 +1,9 @@
 import MattFunct as mf
-#import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as pyplot
 
 print("The Result: ", mf.MattMult(2, 3))
 print("The Result: ", mf.MattDiv(10, 0))
 print("The Result: ", mf.MattDiv(10, 5))
 
-#pyplot.plot((0,1),(1,2))
-#pyplot.show()
+pyplot.plot((0,1),(1,2))
+pyplot.show()
