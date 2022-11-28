e8c6c8cd9ed507be40a146187ceb9925e546aebc
Matthew Berlinic <mjb855@msn.com>
2022-11-28 13:41:34 -0600

    Changing plot array

--- MattMain.py
+++ MattMain.py
@@ -5,5 +5,5 @@ print("The Result: ", mf.MattMult(2, 3))
 print("The Result: ", mf.MattDiv(10, 0))
 print("The Result: ", mf.MattDiv(10, 5))
 
-pyplot.plot((0,1),(1,2))
+pyplot.plot((0,1,2,3),(0,1,3,0))
 pyplot.show()
